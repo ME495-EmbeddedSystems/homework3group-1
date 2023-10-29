6fac853bc68813d3f4ed5660ada74c0ba5860cce
Carter DiOrio <carterdiorio2024@u.northwestern.edu>
2023-10-29 14:27:12 -0500

    added node parameter

--- moveit_wrapper/moveit_wrapper/moveitapi.py
+++ moveit_wrapper/moveit_wrapper/moveitapi.py
@@ -1,7 +1,11 @@
+from rclpy.node import Node
+from rclpy.action import ActionClient
+
+
 class MoveItApi():
     """
     Wraps the moveit ROS API for easy of use
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, node: Node):
+        self.node = node
